922926fd14fefbcdae2c7aeb9b25c671e40300e6
Caio Aranha <caranhatieri@gmail.com>
2024-05-09 11:25:56 -0300

    funcs pt2

--- example.py
+++ example.py
@@ -1,9 +1,11 @@
 # example.py
 import funcs as f
 import cv2
+import easyocr
+import pandas as pd
 
 # Specify paths
-screenshot_path = 'img6.jpg'
+screenshot_path = 'img5.jpg'
 
 image = f.open_resize(screenshot_path)
 
@@ -11,4 +13,7 @@ champion_images = f.get_champion_images(image)
 hud_images = f.get_players_hud(image)
 icons = f.load_icons("championIcons")
 top_matches, list = f.find_best_matches(icons, champion_images)
-print(list)
+
+reader = easyocr.Reader(['en'])
+df = f.get_top_bar(image, reader)
+print(df)
--- funcs.py
+++ funcs.py
@@ -21,7 +21,6 @@ def open_resize(path):
         print("Error: Unable to load image")
     return image
 
-
 def get_champion_images(image):
     xChampionIcon1, yChampionIcon1 = 20, 100
     xChampionIcon2, yChampionIcon2 = 20, 167
@@ -80,7 +79,6 @@ def load_icons(icon_folder):
         icon = cv2.imread(icon_path)
         icons[character_name] = icon
     return icons
-
 
 def compare_images(image1, image2, region_size=20, tolerance=50):
     # Load and resize images
@@ -143,3 +141,197 @@ def find_best_matches(icons_images, champions_images):
 
 
     return top_matches, ordered_champion_list
+
+def crop_bounding_box(processed_image):
+    # Find the bounding box
+    non_zero_pixels = cv2.findNonZero(processed_image)
+    x, y, w, h = cv2.boundingRect(non_zero_pixels)
+    cropped = processed_image[y:y+h, x:x+w]
+    return cropped
+
+def image_processing(image):
+
+    image = image.astype(np.float32) / 255.0
+    # Increase the contrast of the image
+    image = cv2.pow(image, 1.2)
+    # Clip values to the range [0, 1]
+    image = np.clip(image, 0, 1)
+    # Convert back to 8-bits
+    image = (255 * image).astype(np.uint8)
+    image = cv2.resize(image, None, fx=8, fy=8, interpolation=cv2.INTER_CUBIC)
+    # Raise brightness
+    image = cv2.convertScaleAbs(image, alpha=1.1, beta=0)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Use Otsu's method to automatically calculate the threshold value
+    _, binary = cv2.threshold(gray, 110, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    #binary = crop_bounding_box(binary)
+
+
+   
+    return binary
+
+def limpaerrosgold(string):
+    index =0
+    for l in string:
+        
+        if l=='S':
+            string = string[:index] + '5' + string[index+1:]
+        if l=='O':
+            string = string[:index] + '0' + string[index+1:]
+        if l=='o':
+            string = string[:index] + '0' + string[index+1:]
+        if l=='s':
+            string = string[:index] + '2' + string[index+1:]
+        if l=='l':
+            string = string[:index] + '1' + string[index+1:]
+        if l=='Z':
+            string = string[:index] + '2' + string[index+1:]
+        if l=='k':
+            string = string[:index] + string[index+1:]
+        index+=1
+
+    return string
+
+def apaganaonumero(string):
+    index =0
+    for l in string:
+        if l == '_':
+            string.replace(l,'')
+        if l == 'T':
+            string = string[:index] + '1' + string[index+1:]
+        elif (not (l.isdigit())) or (l=='<') or (l=='>'):
+            string.strip(l)
+        
+            
+        index+=1
+    return string
+
+def extract_text(image,reader):
+    result = reader.readtext(image)
+    try:
+        text = result[0][1]
+    except:
+        try:
+            image = crop_bounding_box(image)
+            result = reader.readtext(image)
+            text = result[0][1]
+        except:
+            text = "No text detected"
+    return text
+
+
+def get_top_bar(image,reader):
+    xGold1, yGold1 = 749, 7
+    widthGold, heightGold = 35, 22
+    gold_roi1 = image[yGold1:yGold1+heightGold, xGold1:xGold1+widthGold]
+    gold_roi1 = image_processing(gold_roi1)
+    gold1 = extract_text(gold_roi1,reader)
+    gold1 = limpaerrosgold(gold1)
+    gold1 = apaganaonumero(gold1)
+    gold1.replace('_','')
+    
+
+    
+    #get gold_roi2
+    xGold2, yGold2 = 543, 8
+    gold_roi2 = image[yGold2:yGold2+heightGold, xGold2:xGold2+widthGold]
+    gold_roi2 = image_processing(gold_roi2)
+    gold2 = extract_text(gold_roi2,reader)
+    gold2 = limpaerrosgold(gold2)
+    gold2 = apaganaonumero(gold2)
+
+    #get towers1
+    xTowers1, yTowers1 = 797, 9
+    widthTowers, heightTowers = 23, 19
+    towers1 = image[yTowers1:yTowers1+heightTowers, xTowers1:xTowers1+widthTowers]
+    towers1 = image_processing(towers1)
+    towers1 = extract_text(towers1,reader)
+    towers1 = apaganaonumero(towers1)
+
+    #get towers2
+    xTowers2, yTowers2 = 499, 9
+    towers2 = image[yTowers2:yTowers2+heightTowers, xTowers2:xTowers2+widthTowers]
+    towers2 = image_processing(towers2)
+    towers2 = extract_text(towers2,reader)
+    towers2 = apaganaonumero(towers2)
+
+    #get dragons 1
+    xDragons1, yDragons1 = 951, 0
+    widthDragons, heightDragons = 25, 30
+    dragons1 = image[yDragons1:yDragons1+heightDragons, xDragons1:xDragons1+widthDragons]
+    dragons1 = image_processing(dragons1)
+    dragons1 = extract_text(dragons1,reader)
+    dragons1 = apaganaonumero(dragons1)
+
+    #get dragons 2
+    xDragons2, yDragons2 = 350, 0
+    dragons2 = image[yDragons2:yDragons2+heightDragons, xDragons2:xDragons2+widthDragons]
+    dragons2 = image_processing(dragons2)
+    dragons2 = extract_text(dragons2,reader)
+    dragons2 = apaganaonumero(dragons2)
+
+    #get arauto 1
+    xArauto1, yArauto1 = 899, 3
+    widthArauto, heightArauto = 28, 25
+    Arauto1 = image[yArauto1:yArauto1+heightArauto, xArauto1:xArauto1+widthArauto]
+    Arauto1 = image_processing(Arauto1)
+    arauto1 = extract_text(Arauto1,reader)
+    arauto1 = apaganaonumero(arauto1)
+
+    #get arauto 2
+    xArauto2, yArauto2 = 401, 3
+    Arauto2 = image[yArauto2:yArauto2+heightArauto, xArauto2:xArauto2+widthArauto]
+    Arauto2 = image_processing(Arauto2)
+    arauto2 = extract_text(Arauto2,reader)
+    arauto2 = apaganaonumero(arauto2)
+
+    #get larva1
+    xLarva1, yLarva1 = 845, 9
+    widthLarva, heightLarva = 15, 19
+    larva1 = image[yLarva1:yLarva1+heightLarva, xLarva1:xLarva1+widthLarva]
+    larva1 = image_processing(larva1)
+    larva1 = extract_text(larva1,reader)
+    larva1 = apaganaonumero(larva1)
+
+    #get larva2
+    xLarva2, yLarva2 = 449, 9
+    larva2 = image[yLarva2:yLarva2+heightLarva, xLarva2:xLarva2+widthLarva]
+    larva2 = image_processing(larva2)
+    larva2 = extract_text(larva2,reader)
+    larva2 = apaganaonumero(larva2)
+
+    #get kills1
+    xKills1, yKills1 = 667, 10
+    widthKills, heightKills = 42, 27
+    kills1 = image[yKills1:yKills1+heightKills, xKills1:xKills1+widthKills]
+    kills1 = image_processing(kills1)
+    kills1 = extract_text(kills1,reader)    
+    kills1 = limpaerrosgold(apaganaonumero(kills1))
+
+    #get kills2
+    xKills2, yKills2 = 607, 10
+    widthKills2, heightKills2 = 39, 30
+    kills2 = image[yKills2:yKills2+heightKills2, xKills2:xKills2+widthKills2]
+    kills2 = image_processing(kills2)
+    kills2 = extract_text(kills2,reader)
+    kills2 = limpaerrosgold(apaganaonumero(kills2))
+
+    tabela = pd.DataFrame()
+    tabela["TIME"] = ["RED", "BLUE"]
+    tabela["GOLD"] = [gold1, gold2]
+    tabela["TOWERS"] = [towers1, towers2]
+    tabela["DRAGONS"] = [dragons1, dragons2]
+    tabela["ARAUTO"] = [arauto1, arauto2]
+    tabela["LARVA"] = [larva1, larva2]
+    tabela["KILLS"] = [kills1, kills2]
+
+    return tabela
+
+
+
+
+
+
+
