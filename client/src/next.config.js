694d410e5fb055466157d302eff92d9915c4b00e
Leonardo <leofmandrade@gmail.com>
2024-06-03 20:33:20 -0300

    aa

--- client/src/next.config.js
+++ client/src/next.config.js
@@ -1,25 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/geturl',
-        permanent: true, // ou false, dependendo se você quer um redirecionamento permanente ou temporário
-      },
-    ]
-  },
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/:path*',
-        destination:
-          process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:5328/api/:path*'
-            : '/api/',
-      },
-    ]
-  },
-}
-
-module.exports = nextConfig
+    rewrites: async () => {
+      return [
+        {
+          source: '/api/:path*',
+          destination:
+            process.env.NODE_ENV === 'development'
+              ? 'http://127.0.0.1:5328/api/:path*'
+              : '/api/',
+        },
+      ]
+    },
+  }
+  
+  module.exports = nextConfig
